1f9a5a905a914de514bb3bc39a756ce109c39b12
张兆华 <593245631@qq.com>
2019-05-07 09:36:09 +0800

    界面

---
+++ 贪吃蛇/贪吃蛇/test.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+using namespace std;
+
+void menu()
+{
+	cout << "******************1. PLAY *********************" << endl;
+	cout << "******************2. TOP  *********************" << endl;
+	cout << "******************0. EXIT *********************" << endl;
+	cout << "***********************************************" << endl;
+}
+void game()
+{
+	//cout << "game()" << endl;
+
+}
+
+void top()
+{
+	cout << "top()" << endl;
+}
+int main()
+{
+	int ret = 1;
+
+	do {
+		menu();
+		cin >> ret;
+		switch(ret)
+		{
+		case 1:
+			game();
+			break;
+		case 2:
+			top();
+		case 0:
+			break;
+		default:
+			cout << "ÊäÈë´íÎó" << endl;
+			break;
+		}
+	} while (ret);
+	return 0;
+}
